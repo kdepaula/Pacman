ab76919139defe15fc8344e18097a2df81e03909
kdepaula <42978472+kdepaula@users.noreply.github.com>
2019-05-30 14:28:43 -0400

    Add files via upload

--- PacMan.java
+++ PacMan.java
@@ -1,4 +1,38 @@
+	import java.awt.Graphics;
+	import java.awt.Graphics2D;
+	import java.awt.geom.Ellipse2D;
+	import java.awt.geom.Rectangle2D;
+	import javax.swing.JComponent;
+import javax.swing.JLabel;
 
-public class PacMan {
+public class PacMan extends JLabel
+{
+		private int dx;
+		private int dy;
+		
+		public void update()
+		{
+			setLocation(getX() + dx, getY() + dy);
+		}
+		
+		public void setDx(int a)
+		{
+			dx = a;
+		}
+		
+		public void setDy(int a)
+		{
+			dy = a;
+		}
 
+		public int getDx() 
+		{
+			return dx;
+		}
+		
+		public int getDy()
+		{
+			return dy;
+		}
+		
 }
